bf12f20cc857e78aaa0ff69ca0e8de00b47c5dcf
Sebastián Guzmán <20200981@aloe.ulima.edu.pe>
2025-10-03 00:56:10 -0500

    nivel Sebastian

---
+++ Assets/scripts/PlayerDetectionHit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerDetectionHit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Si colisionamos con la zona de muerte "DeadZone" o colisionamos con un enemigo
+        if (collision.gameObject.CompareTag("DeadZone"))
+        {
+            SpawnPlayer();
+        }
+    }
+
+    private void SpawnPlayer()
+    {
+        // Ubicamos el SpawnPoint, eso significa que el spawnpoint debe tener su etiqueta (tag)
+        GameObject spawn = GameObject.FindGameObjectWithTag("SpawnPoint");
+        // Mandamos al player a esa posici�n.
+        transform.localPosition = spawn.transform.localPosition;
+    }
+}
