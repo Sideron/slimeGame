4233c2947c3691c2fc4f056b0befcf1030baf9c0
Sideron <66834276+Sideron@users.noreply.github.com>
2025-10-03 16:39:32 -0500

    Late fixes

--- Assets/scripts/cameraFollow.cs
+++ Assets/scripts/cameraFollow.cs
@@ -10,9 +10,9 @@ public class cameraFollow : MonoBehaviour
         Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Vector2.Distance(mouseWorld,targetPoint.transform.position) < 2)
         {
-            followPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            followPoint = targetPoint.transform.position;
         }
-        Vector2 nPosition = Vector2.Lerp(transform.position,followPoint,speed*Time.deltaTime);
+        Vector2 nPosition = Vector2.Lerp(transform.position,followPoint+Vector2.up*4+Vector2.right*3,speed*Time.deltaTime);
         //Vector2 nPosition = followPoint;
         transform.position = new Vector3(nPosition.x, nPosition.y,-10);
     }
