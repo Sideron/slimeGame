19a08f4ecdf08aa98289ffbdbbc14ff7a7d079a0
Sideron <66834276+Sideron@users.noreply.github.com>
2025-10-10 09:38:06 -0500

    ajustes en jumper

--- Assets/scripts/slimeScripts/JumpSlime.cs
+++ Assets/scripts/slimeScripts/JumpSlime.cs
@@ -6,10 +6,13 @@ public class JumpSlime : Slime
     private float impulseForce = 10;
     public override void onTouch(Rigidbody2D rb)
     {
-        if (rb.linearVelocity.y != 0)
+        if (rb.linearVelocity.y > 0.01f || rb.linearVelocity.y <-0.01f)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
-            rb.AddForce((-transform.position + rb.transform.position).normalized * impulseForce * 10, ForceMode2D.Impulse);
+            float acutalSpeed = rb.linearVelocity.magnitude;
+            Debug.Log("Actual speed: " + acutalSpeed);
+            rb.linearVelocity = (-transform.position + rb.transform.position).normalized*(acutalSpeed>=impulseForce?acutalSpeed:impulseForce);
+            //rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+            //rb.AddForce((-transform.position + rb.transform.position).normalized * impulseForce * 10, ForceMode2D.Impulse);
         }
     }
     public override void onRelease(Rigidbody2D rb){}
