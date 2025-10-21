24a0f58d5dc88efa0a2d7934cc8efc2d7a1743a4
Sideron <66834276+Sideron@users.noreply.github.com>
2025-10-21 16:52:46 -0500

    slime count function

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -4,10 +4,14 @@ public class GameManager : MonoBehaviour
 {
     public GameObject[] slimes;
     public int[] amounts;
+    private int[] currentAmounts;
     public playerShoot player;
+    public slimeRow row;
     int currentIndex = 0;
     void Start()
     {
+        player.gm = this;
+        currentAmounts = amounts;
         player.setCharge(slimes[currentIndex]);
     }
 
@@ -31,6 +35,15 @@ public class GameManager : MonoBehaviour
         currentIndex = (currentIndex + direction + slimes.Length) % slimes.Length;
 
         player.setCharge(slimes[currentIndex]);
+        row.setIndex(currentIndex);
         Debug.Log("Switched to slime index: " + currentIndex);
+    }
+    public bool canShoot(){ return currentAmounts[currentIndex] > 0; }
+    public void reduceSlimeCount()
+    {
+        if (canShoot())
+        {
+            currentAmounts[currentIndex] = currentAmounts[currentIndex] - 1;
+        }
     }
 }
--- Assets/scripts/playerShoot.cs
+++ Assets/scripts/playerShoot.cs
@@ -4,6 +4,7 @@ public class playerShoot : MonoBehaviour
 {
     private GameObject slimeCharge;
     public float projectileSpeed = 10f;
+    public GameManager gm;
     GunAim ga;
     void Start()
     {
@@ -21,18 +22,22 @@ public class playerShoot : MonoBehaviour
 
     void Shoot()
     {
-        // Asegúrate de que GunAim te dé la posición y dirección de disparo
-        Vector2 shootPos = ga.getShootPosition();   // posición desde donde disparar
-        Vector2 shootDir = ga.getShootDirection();  // dirección hacia donde disparar
-
-        // Crear el proyectil
-        GameObject proj = Instantiate(slimeCharge, transform.position, Quaternion.identity);
-
-        // Aplicar velocidad
-        Rigidbody2D rb = proj.GetComponent<Rigidbody2D>();
-        if (rb != null)
+        if (gm.canShoot())
         {
-            rb.linearVelocity = shootDir.normalized * projectileSpeed;
+            // Asegúrate de que GunAim te dé la posición y dirección de disparo
+            Vector2 shootPos = ga.getShootPosition();   // posición desde donde disparar
+            Vector2 shootDir = ga.getShootDirection();  // dirección hacia donde disparar
+
+            // Crear el proyectil
+            GameObject proj = Instantiate(slimeCharge, transform.position, Quaternion.identity);
+
+            // Aplicar velocidad
+            Rigidbody2D rb = proj.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.linearVelocity = shootDir.normalized * projectileSpeed;
+            }
+            gm.reduceSlimeCount();
         }
     }
     public void setCharge(GameObject slime)
