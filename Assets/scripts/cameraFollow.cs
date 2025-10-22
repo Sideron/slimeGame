d5ad1c9b4edeab37fa52aa3317c95f7ee5c26da3
Sideron <66834276+Sideron@users.noreply.github.com>
2025-10-22 03:35:50 -0500

    camera adjustments

--- Assets/scripts/cameraFollow.cs
+++ Assets/scripts/cameraFollow.cs
@@ -1,28 +1,42 @@
 using UnityEngine;
-
-public enum CameraDirection
-{
-    Horizontal,
-    Vertical
-}
 public class cameraFollow : MonoBehaviour
 {
     public Transform targetPoint;
     public float min = -5;
     public float max = 5;
     [SerializeField]
-    private CameraDirection direction;
+    private bool freezeY = true;
     public float speed = 10;
+    public float differenceX = 3f;
+    public float offsetFactor = 0.025f;
+
+    private float pointerDiff = 0f;
+    private float lastX = 0f;
     void FixedUpdate()
     {
-        Vector2 nPosition = Vector2.Lerp(transform.position, targetPoint.position, speed * Time.deltaTime);
-        if (direction == CameraDirection.Horizontal)
+        float diff = targetPoint.position.x - lastX;
+        float relative = (diff!=0? Mathf.Sign(diff):0) * offsetFactor;
+
+        pointerDiff = Mathf.Clamp(pointerDiff + relative, -1f, 1f);
+
+        lastX = targetPoint.position.x;
+
+        float targetX = Mathf.Clamp(
+            targetPoint.position.x + (differenceX * pointerDiff),
+            min,
+            max
+        );
+
+        float smoothX = Mathf.Lerp(transform.position.x, targetX, speed * Time.deltaTime);
+
+        transform.position = new Vector3(smoothX, transform.position.y, -10f);
+        /*if (freezeY)
         {
             transform.position = new Vector3(Mathf.Clamp(nPosition.x,min,max), transform.position.y, -10);
         }
         else
         {
             transform.position = new Vector3(transform.position.x, Mathf.Clamp(nPosition.y,min,max), -10);
-        }
+        }*/
     }
 }
