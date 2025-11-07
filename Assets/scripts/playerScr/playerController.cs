164a260bdea736e633c4efeefc286001876f308d
Sebastián Guzmán <20200981@aloe.ulima.edu.pe>
2025-11-07 02:42:31 -0500

    amanecida de SG

--- Assets/scripts/playerScr/playerController.cs
+++ Assets/scripts/playerScr/playerController.cs
@@ -12,17 +12,22 @@ public class playerController : MonoBehaviour
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
+    private Animator anim;
+
     public bool isGrounded;
     private float moveInput;
-    public float coyoteTime = 0.1f;   // tiempo extra después de caer
+    public float coyoteTime = 0.1f;
     private float coyoteTimeCounter;
+
     [SerializeField] private AudioClip[] jumpSound;
     [SerializeField] private AudioClip pisadas;
-    [SerializeField] private float stepInterval = 0.1f; // tiempo entre pasos
+    [SerializeField] private float stepInterval = 0.1f;
     private float stepTimer = 0f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     void Update()
@@ -30,11 +35,15 @@ public class playerController : MonoBehaviour
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         coyoteTimeCounter = isGrounded ? coyoteTime : coyoteTimeCounter - Time.deltaTime;
         moveInput = Input.GetAxisRaw("Horizontal");
+
+        anim.enabled = (isGrounded && Mathf.Abs(moveInput) > 0);
+
         if (moveInput != 0)
         {
             if (isGrounded)
             {
                 rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
                 stepTimer -= Time.deltaTime;
                 if (stepTimer <= 0f)
                 {
@@ -44,7 +53,10 @@ public class playerController : MonoBehaviour
             }
             else
             {
-                rb.linearVelocity = new Vector2(Mathf.Lerp(rb.linearVelocity.x, moveInput * moveSpeed, 5f * Time.deltaTime), rb.linearVelocity.y);
+                rb.linearVelocity = new Vector2(
+                    Mathf.Lerp(rb.linearVelocity.x, moveInput * moveSpeed, 5f * Time.deltaTime),
+                    rb.linearVelocity.y
+                );
             }
         }
         else if (isGrounded)
@@ -52,26 +64,23 @@ public class playerController : MonoBehaviour
             stepTimer = 0f;
             rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
         }
-        if (Input.GetButtonDown("Jump"))
+
+        if (Input.GetButtonDown("Jump") && coyoteTimeCounter > 0)
         {
-            if (coyoteTimeCounter > 0)
-            {
-                AudioManager.Instance.PlayRandomSFX(jumpSound);
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-                coyoteTimeCounter = 0;
-            }
+            AudioManager.Instance.PlayRandomSFX(jumpSound);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            coyoteTimeCounter = 0;
         }
     }
-    
+
     public void Dash()
     {
         Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        rb.linearVelocity = new Vector2((mouseWorld-transform.position).normalized.x*jumpForce , jumpForce);
+        rb.linearVelocity = new Vector2((mouseWorld - transform.position).normalized.x * jumpForce, jumpForce);
     }
 
     void OnDrawGizmosSelected()
     {
-        // Visualiza el groundCheck en la escena
         if (groundCheck != null)
         {
             Gizmos.color = Color.red;
