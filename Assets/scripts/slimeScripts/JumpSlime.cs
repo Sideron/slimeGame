70d0f28c23d29cdce5b1b6d58000599cf0a32396
Sideron <66834276+Sideron@users.noreply.github.com>
2025-11-07 12:21:29 -0500

    jump slime anim

--- Assets/scripts/slimeScripts/JumpSlime.cs
+++ Assets/scripts/slimeScripts/JumpSlime.cs
@@ -6,9 +6,16 @@ public class JumpSlime : Slime
     [SerializeField]
     private float impulseForce = 10;
     [SerializeField] private AudioClip[] jump;
+    private Animator myAnim;
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+        Invoke("animUnabled", 0.4f);
+        Debug.Log(myAnim.name);
+    }
     public override void onTouch(Rigidbody2D rb)
     {
-        base.onShoot(rb);
+        base.onTouch(rb);
         if (rb.linearVelocity.y > 0.01f || rb.linearVelocity.y < -0.01f)
         {
             pushObject(rb);
@@ -24,11 +31,22 @@ public class JumpSlime : Slime
             pushObject(rb);
         }
     }
-    
+
     private void pushObject(Rigidbody2D rb)
     {
         AudioManager.Instance.PlayRandomSFX(jump);
+        ;
+        if (myAnim)
+        {
+            myAnim.enabled = true;
+            Invoke("animUnabled", 0.5f);
+        } // nombre del state, no del clip
         float acutalSpeed = rb.linearVelocity.magnitude;
         rb.linearVelocity = (-transform.position + rb.transform.position).normalized * (acutalSpeed >= impulseForce ? acutalSpeed : impulseForce);
+    }
+    void animUnabled()
+    {
+        myAnim.Rebind();
+        myAnim.enabled = false;
     }
 }
